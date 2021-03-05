4fd0648a9317687391af56e137ff1a35e2af13eb
Johnathan <johnathanboyd5@gmail.com>
2021-03-05 16:36:04 -0600

    3rd commit

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,7 @@
 $( document ).ready( onReady );
 
 let employees = [];
+
 function onReady(){
     console.log( 'JQ' )
     //click event handlers
@@ -9,8 +10,23 @@ function onReady(){
 
 function submitInfo(){
     console.log('in submitInfo');
+    const fName = $( '#firstName' ).val();
+    const lName = $( '#lastName' ).val();
+    const idNum = $( '#idNum' ).val();
+    const jTitle = $( '#jobTitle' ).val();
+    const annualSal = $( '#annualSalary' ).val();
     // create a new object with each submit
-    //push that ovject into the '!!!!!array!!!!!'
-    //update DOM
-    //empty text input
+    let newObject = {
+        firstName: fName,
+        lastName: lName,
+        idNumber: idNum,
+        jobTitle: jTitle,
+        annualSalaray: annualSal
+    } // end newObject
+    // push that object into the employees
+    employees.push( newObject );
+    // update DOM
+    //showSubmit();
+    // empty text input
+    $( '#firstName' ).val( '' );
 } // end submitInfo
